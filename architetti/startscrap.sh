75f126fb47c6f92edcc9bf83c7ddffe61fc58206
marco benatti <marco.benatti@gmail.com>
2024-05-21 19:03:36 +0200

    raspberry daemon start batch file

---
+++ architetti/startscrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export EUROPACONCORSI_USERNAME="mattia.benatti@gmail.com"
+export EUROPACONCORSI_PASSWORD="Aittam90!"
+export TELEGRAM_BOT_TOKEN="771229754:AAEiwhrNj5Ux7Ut4bpo8ndkfx94faGQhltk"
+cd /home/benatti/repos/RSS-to-Telegram-Bot/architetti
+#source chatGPT/bin/activate
+python architetti.py
+
